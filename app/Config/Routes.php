e16811fc84123984a77e7286bb77856096226089
dhshanuka <dhshanuka@gmail.com>
2024-11-05 11:37:12 +0530

    add courses

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -9,4 +9,5 @@ $routes->get('/', 'Home::index');
 $routes->get('add_course', 'Addcourse::index');
 $routes->get('course', 'Course::index');
 $routes->get('coursemanagement', 'Coursemanagement::index');
+$routes->get('course/year1', 'Courseyear1::index');
 
